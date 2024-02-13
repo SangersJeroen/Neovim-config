d7ad272ee272840ad707570a97d96bb1f08a8cb1
SangersJeroen <jeroen98.sangers@gmail.com>
2024-02-13 20:49:47 +0100

    removed redundant keymap

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -20,7 +20,6 @@ return {
                     vim.keymap.set("n", "<C-x>", function() vim.lsp.buf.code_action() end, opts)
                     vim.keymap.set("n", "<Space>c", function() vim.diagnostic.goto_prev() end, opts)
                     vim.keymap.set("n", "<Space>v", function() vim.diagnostic.goto_next() end, opts)
-                    vim.keymap.set("n", "<M-p>", function() vim.lsp.buf.format() end)
                     vim.keymap.set('n', '<space>wa', function() vim.lsp.buf.add_workspace_folder() end, opts)
                     vim.keymap.set('n', '<space>wr', function() vim.lsp.buf.remove_workspace_folder() end, opts)
                     vim.keymap.set('n', '<space>D', function() vim.lsp.buf.type_definition() end, opts)
