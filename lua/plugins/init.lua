2cd9977453599b526ffd73fac9249a8d4fdf1099
SangersJeroen <jeroen98.sangers@gmail.com>
2024-02-29 14:21:40 +0100

    added harpooon files

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -131,10 +131,14 @@ return {
             vim.keymap.set('n', '<leader>a', mark.add_file)
             vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
 
-            vim.keymap.set('n', '<M-q>', function() ui.nav_file(1) end)
-            vim.keymap.set('n', '<M-w>', function() ui.nav_file(2) end)
-            vim.keymap.set('n', '<M-e>', function() ui.nav_file(3) end)
-            vim.keymap.set('n', '<M-r>', function() ui.nav_file(4) end)
+            vim.keymap.set('n', '<M-1>', function() ui.nav_file(1) end)
+            vim.keymap.set('n', '<M-2>', function() ui.nav_file(2) end)
+            vim.keymap.set('n', '<M-3>', function() ui.nav_file(3) end)
+            vim.keymap.set('n', '<M-4>', function() ui.nav_file(4) end)
+            vim.keymap.set('n', '<M-q>', function() ui.nav_file(5) end)
+            vim.keymap.set('n', '<M-w>', function() ui.nav_file(6) end)
+            vim.keymap.set('n', '<M-e>', function() ui.nav_file(7) end)
+            vim.keymap.set('n', '<M-r>', function() ui.nav_file(8) end)
         end,
     },
 
