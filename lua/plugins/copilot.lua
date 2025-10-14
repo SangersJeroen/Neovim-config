5b74cd90a26faa3f6b55e090965146ea49681904
SangersJeroen <jeroen98.sangers@gmail.com>
2025-10-14 17:24:49 +0200

    changed copilot key from Alt+A to Ctrl+A to remove overlap with Codeium

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,10 +3,10 @@ return {
         "github/copilot.vim",
         config = function()
             vim.cmd("Copilot disable")
-            vim.keymap.set('i', '<M-a>', 'copilot#Accept("\\<CR>")',
+            vim.keymap.set('i', '<C-a>', 'copilot#Accept("\\<CR>")',
                 {
                     expr = true,
-                    replace_leycodes = false
+                    replace_keycodes = false
                 })
             vim.g.copilot_no_tab_map = true
         end
