093ea9ac5965856680296f0dffca2fbba0b185c9
SangersJeroen <jeroen98.sangers@gmail.com>
2026-01-26 23:26:16 +0100

    Experimenting with git utilities

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,26 +1,26 @@
 return {
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require('gitsigns').setup()
-            local gs = package.loaded.gitsigns
-            vim.keymap.set('n', '<leader>hs', gs.stage_hunk, { desc = 'stage chunk' })
-            vim.keymap.set('n', '<leader>hr', gs.reset_hunk, { desc = 'reset chunk' })
-            vim.keymap.set('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
-                { desc = 'stage selected' })
-            vim.keymap.set('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
-                { desc = 'reset selected' })
-            vim.keymap.set('n', '<leader>hS', gs.stage_buffer, { desc = 'stage file' })
-            vim.keymap.set('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage' })
-            vim.keymap.set('n', '<leader>hR', gs.reset_buffer, { desc = 'reset file' })
-            vim.keymap.set('n', '<leader>hp', gs.preview_hunk, { desc = 'preview chunk' })
-            vim.keymap.set('n', '<leader>hb', function() gs.blame_line { full = true } end, { desc = 'full blame' })
-            vim.keymap.set('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle blame' })
-            vim.keymap.set('n', '<leader>hd', gs.diffthis, { desc = 'diff this' })
-            vim.keymap.set('n', '<leader>hD', function() gs.diffthis('~') end, { desc = 'diff this' })
-            vim.keymap.set('n', '<leader>td', gs.toggle_deleted, { desc = 'show deleted' })
-        end
-    },
+    -- {
+    --     "lewis6991/gitsigns.nvim",
+    --     config = function()
+    --         require('gitsigns').setup()
+    --         local gs = package.loaded.gitsigns
+    --         vim.keymap.set('n', '<leader>hs', gs.stage_hunk, { desc = 'stage chunk' })
+    --         vim.keymap.set('n', '<leader>hr', gs.reset_hunk, { desc = 'reset chunk' })
+    --         vim.keymap.set('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
+    --             { desc = 'stage selected' })
+    --         vim.keymap.set('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
+    --             { desc = 'reset selected' })
+    --         vim.keymap.set('n', '<leader>hS', gs.stage_buffer, { desc = 'stage file' })
+    --         vim.keymap.set('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage' })
+    --         vim.keymap.set('n', '<leader>hR', gs.reset_buffer, { desc = 'reset file' })
+    --         vim.keymap.set('n', '<leader>hp', gs.preview_hunk, { desc = 'preview chunk' })
+    --         vim.keymap.set('n', '<leader>hb', function() gs.blame_line { full = true } end, { desc = 'full blame' })
+    --         vim.keymap.set('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle blame' })
+    --         vim.keymap.set('n', '<leader>hd', gs.diffthis, { desc = 'diff this' })
+    --         vim.keymap.set('n', '<leader>hD', function() gs.diffthis('~') end, { desc = 'diff this' })
+    --         vim.keymap.set('n', '<leader>td', gs.toggle_deleted, { desc = 'show deleted' })
+    --     end
+    -- },
     {
         "jiaoshijie/undotree",
         ---@module 'undotree.collector'
@@ -66,10 +66,12 @@ return {
                 end,
                 -- Check diff of a commit
                 on_select_commit = function(commit)
+                    require('diffview')
                     vim.cmd(':DiffviewOpen ' .. commit.hash .. '^!')
                 end,
                 -- Check diff from commit a -> commit b
                 on_select_range_commit = function(from, to)
+                    require('diffview')
                     vim.cmd(':DiffviewOpen ' .. from.hash .. '~1..' .. to.hash)
                 end,
             },
@@ -96,5 +98,25 @@ return {
         keys = {
             { "<leader>ng", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
         }
-    }
+    },
+    {
+        'sindrets/diffview.nvim',
+        lazy = true,
+        opts = {
+            hooks = {
+                diff_buf_read = function(bufnr)
+                    -- Change local options in diff buffers
+                    vim.opt_local.wrap = false
+                    vim.opt_local.list = false
+                    vim.opt_local.colorcolumn = { 80 }
+                end,
+                view_opened = function(view)
+                    print(
+                        ("A new %s was opened on tab page %d!")
+                        :format(view.class:name(), view.tabpage)
+                    )
+                end,
+            },
+        },
+    },
 }
